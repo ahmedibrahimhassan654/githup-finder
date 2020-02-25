7a380ab6c80e0aac55776cc59bd5dab663ee6b25
= <39348867+ahmedibrahimhassan654@users.noreply.github.com>
2020-02-25 19:55:24 +0200

    Lesson 12. HTTP Requests & Updating State

--- src/components/users/UserItems.js
+++ src/components/users/UserItems.js
@@ -2,14 +2,15 @@
 import React  from 'react'
 import PropTypes from 'prop-types'
 
- const UserItems=({user:{logIn,avatar_url,html_url}})=>  {
+ const UserItems=({user:{login,avatar_url,html_url}})=>  {
 
    
         return (
             <div className='card text-center' >
                 <img src={avatar_url} alt='' className='round-img' style={{width:'100px'}}></img>
-                <h3>{logIn}</h3> 
+                <h3>{login}</h3> 
                 <div>
+                
                 <a className="btn btn-primary btn-sm my-1 " href={html_url} role="button">show profile</a>
                 
                 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,38 +3,11 @@ import UserItems from './UserItems'
 
  class Users extends Component {
 
-     state={
-        users:[
-            {
-                id:'1',
-                logIn:'mojombo',
-                avatar_url:'https://avatars0.githubusercontent.com/u/1?v=4',
-                html_url:'https://github.com/mojombo'
-        
-            },
-            {
-                id:'2',
-                logIn:'defunkt',
-                avatar_url:'https://avatars0.githubusercontent.com/u/2?v=4',
-                html_url:'https://github.com/defunkt'
-        
-            }, 
-             {
-                id:'3',
-                logIn:'pjhyett',
-                avatar_url:'https://avatars0.githubusercontent.com/u/3?v=4',
-                html_url:'https://github.com/pjhyett'
-        
-            }
-
-
-
-        ]
-     }
+    
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user=>(
+                {this.props.users.map(user=>(
                 <UserItems key={user.id} user={user}/>
                 ))}
             </div>
@@ -43,7 +16,7 @@ import UserItems from './UserItems'
 }
 const userStyle={
     display: 'grid',
-    gridTemplateColumns: 'repeat(3,1fr)',
+    gridTemplateColumns: 'repeat(4,1fr)',
     gridGap:'1rem',
    
 }
